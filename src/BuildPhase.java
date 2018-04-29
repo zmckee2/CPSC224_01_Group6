b9b4b9fd43eb8084fffc954a06b3119da05e576a
papahance98 <khance@zagmail.gonzaga.edu>
2018-04-28 19:06:55 -0700

    Added a work-in-progress of the GUI implementation for BuildPhase, still need to finalize implementation, optimize and add comments.

--- src/BuildPhase.java
+++ src/BuildPhase.java
@@ -1,36 +1,191 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.swing.*;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.*;
+import java.awt.color.*;
 
-public class BuildPhase implements JFrame{
+
+
+
+public class BuildPhase extends JPanel{
 	
 	Hand[] players;
 	int numPlayers;
 	
-	public static void main(String[] args){
-		BuildPhase b = new BuildPhase(Hand );
-	}
+	JButton ckpt1,ckpt2,ckpt3,thrst1,thrst2,thrst3,fres1,fres2,fres3,
+			addCrewMem,addFuel,die1,die2,die3,die4,die5,reroll,proceed;
+	
+	JLabel numCrewMem,maxCrewMem,numFuel;
+	
+	//JButton[] btns;
+	
+	//JButton[] dice;
+	
+	Dimension dieDim = new Dimension(50,50);
+	
+	Color red_btn = new Color(239,95,95);
+	Color blu_btn = new Color(111, 220, 237);
+	Color grn_btn = new Color(97,244,132);
 	
 	public BuildPhase(Hand[] initPlayers){
 		players = initPlayers;
 		numPlayers = players.length;
 		
-		JPanel panel = new JPanel();
+		this.setLayout(new BorderLayout());
+		JPanel scrb = new JPanel();
+		scrb.setLayout(new GridBagLayout());
+		JPanel dicePanel = new JPanel();
+		dicePanel.setLayout(new GridBagLayout());
+		
+		GridBagConstraints gCon = new GridBagConstraints();
+		
+		gCon.gridx = 1;
+		gCon.gridy = 1;
+		gCon.gridwidth = 1;
+		gCon.gridheight = 1;
+		gCon.weightx = 1;
+		gCon.weighty = 1;
+		gCon.insets = new Insets(10,10,10,10);
+		gCon.anchor = GridBagConstraints.CENTER;
+		gCon.fill = GridBagConstraints.BOTH;
 		
 		
 		
+		
+		numCrewMem = new JLabel("Number of Crew Members: 0");
+		maxCrewMem = new JLabel("Max Number of Crew Members: 0");
+		addCrewMem = new JButton("Add Crew Member");
+		numFuel = new JLabel("Units of Fuel: 0");
+		addFuel = new JButton("Add Fuel");
+		ckpt1 = new JButton("Cockpit Mk1");
+		ckpt2 = new JButton("Cockpit Mk2");
+		ckpt3 = new JButton("Cockpit Mk3");
+		thrst1 = new JButton("Thruster Mk1");
+		thrst2 = new JButton("Thruster Mk2");
+		thrst3 = new JButton("Thruster Mk3");
+		fres1 = new JButton("Fuel Reserves Mk1");
+		fres2 = new JButton("Fuel Reserves Mk2");
+		fres3 = new JButton("Fuel Reserves Mk3");
+		
+		die1 = new JButton("ELEC");
+		die2 = new JButton("CREW");
+		die3 = new JButton("FUEL");
+		die4 = new JButton("HULL");
+		die5 = new JButton("GLAS");
+		reroll = new JButton("REROLL");
+		proceed = new JButton("PROCEED TO SPACE PHASE");
+		
+		/*JButton[] btns = {ckpt1,ckpt2,ckpt3,thrst1,thrst2,thrst3,fres1,fres2,fres3,
+				addCrewMem,addFuel,die1,die2,die3,die4,die5,reroll,proceed};
+		
+		JButton[] dice = {die1,die2,die3,die4,die5};*/
+		
+		ListenForButton lForBtn = new ListenForButton();
+		
+		ckpt1.addActionListener(lForBtn);
+		ckpt2.addActionListener(lForBtn);
+		ckpt3.addActionListener(lForBtn);
+		thrst1.addActionListener(lForBtn);
+		thrst2.addActionListener(lForBtn);
+		thrst3.addActionListener(lForBtn);
+		fres1.addActionListener(lForBtn);
+		fres2.addActionListener(lForBtn);
+		fres3.addActionListener(lForBtn);
+		die1.addActionListener(lForBtn);
+		die2.addActionListener(lForBtn);
+		die3.addActionListener(lForBtn);
+		die4.addActionListener(lForBtn);
+		die5.addActionListener(lForBtn);
+		proceed.addActionListener(lForBtn);
+		reroll.addActionListener(lForBtn);
+		addCrewMem.addActionListener(lForBtn);
+		addFuel.addActionListener(lForBtn);
+		
+		gCon.gridx = 1;
+		scrb.add(ckpt1,gCon);
+		gCon.gridx = 2;
+		scrb.add(thrst1,gCon);
+		gCon.gridx = 3;
+		scrb.add(fres1,gCon);
+		gCon.gridx = 1;
+		gCon.gridy = 2;
+		scrb.add(ckpt2,gCon);
+		gCon.gridx = 2;
+		scrb.add(thrst2,gCon);
+		gCon.gridx = 3;
+		scrb.add(fres2,gCon);
+		gCon.gridx = 1;
+		gCon.gridy = 3;
+		scrb.add(ckpt3,gCon);
+		gCon.gridx = 2;
+		scrb.add(thrst3,gCon);
+		gCon.gridx = 3;
+		scrb.add(fres3,gCon);
+		
+		gCon.gridx = 4;
+		gCon.gridy = 1;
+		scrb.add(numCrewMem,gCon);
+		gCon.gridy = 2;
+		scrb.add(maxCrewMem,gCon);
+		gCon.gridy = 3;
+		scrb.add(addCrewMem,gCon);
+		gCon.gridx = 5;
+		gCon.gridy = 1;
+		scrb.add(numFuel,gCon);
+		gCon.gridy = 3;
+		scrb.add(addFuel,gCon);
+		gCon.gridx = 3;
+		gCon.gridy = 4;
+		scrb.add(reroll, gCon);
+		gCon.gridx = 4;
+		scrb.add(proceed, gCon);
+		
+		
+		gCon.gridx = 1;
+		gCon.gridy = 2;
+		dicePanel.add(die1,gCon);
+		gCon.gridx = 2;
+		dicePanel.add(die2,gCon);
+		gCon.gridx = 3;
+		dicePanel.add(die3,gCon);
+		gCon.gridx = 4;
+		dicePanel.add(die4,gCon);
+		gCon.gridx = 5;
+		dicePanel.add(die5,gCon);
+		
+		this.updateBuildOptions(players[0]);
+		
+		//proceed.setVisible(false);// set PROCEED button to be hidden
+		die1.setVisible(true);
+		scrb.setVisible(true);
+		dicePanel.setVisible(true);
+		this.add(scrb, BorderLayout.NORTH);
+		this.add(dicePanel, BorderLayout.CENTER);
+		this.setVisible(true);
 	}
 	
-	public void playBuild(Hand[] hArr){ // this is basically the main method of BuildPhase
+	public void playBuild(){ // this is basically the main method of BuildPhase
 		for(int i = 0; i < 10; i++){//loop for 10 turns
-			for(int j = 0; j < 4; j++){
+			for(int j = 0; j < players.length; j++){
 				// roll 3 times
-				boolean continueRolling;
+				boolean continueRolling = true;
 				for(int k = 0; k < 3 && continueRolling; k++){
+					players[j].reroll(0);
+					players[j].reroll(1);
+					players[j].reroll(2);
+					players[j].reroll(3);
+					players[j].reroll(4);
 					//if the player doesn't want to roll again, set continueRolling to false
 					rollResources(players[j]);
 				}
-				printBuildOptions(players[j]);
+				updateBuildOptions(players[j]);
 				
 			}
 		}
@@ -42,9 +197,8 @@ public class BuildPhase implements JFrame{
 		}
 	}
 	
-	private void swingInit(){
-		this.setSize(400); // parameter
-		this.setLocationRelativeTo(null);
+	/*private void swingInit(){
+		this.setSize(700, 700); // parameter
 		this.setVisible(true);
 		
 		Toolkit tk = Toolkit.getDefaultToolkit;
@@ -58,36 +212,57 @@ public class BuildPhase implements JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		this.setTitle("SPACE RACE: Build Phase");
-	}
+	}*/
 	
-	private void printBuildOptions(Hand h){
+	private void updateBuildOptions(Hand h){
 		if(canBuildCockpit(h)){
-			//cockpit can be built (UI stuff)
+			// Cockpit can be built
 			if(!h.checkPartBuilt(6)){
-				System.out.println("Cockpit Mk1 can be built");
+				ckpt1.setBackground(grn_btn);
 			} else if(!h.checkPartBuilt(7)){
-				System.out.println("Cockpit Mk2 can be built");
+				ckpt1.setBackground(blu_btn);
+				ckpt2.setBackground(grn_btn);
 			} else if(!h.checkPartBuilt(8)){
-				System.out.println("Cockpit Mk3 can be built");
+				ckpt1.setBackground(blu_btn);
+				ckpt2.setBackground(blu_btn);
+				ckpt3.setBackground(grn_btn);
 			}
+		} else {
+			ckpt1.setBackground(red_btn);
+			ckpt2.setBackground(red_btn);
+			ckpt3.setBackground(red_btn);
 		} if(canBuildThruster(h)){
-			//Thruster can be built (UI stuff)
-			if(!h.checkPartBuilt(0)){
-				System.out.println("Thruster Mk1 can be built");
-			} else if(!h.checkPartBuilt(1)){
-				System.out.println("Thruster Mk2 can be built");
-			} else if(!h.checkPartBuilt(2)){
-				System.out.println("Thruster Mk3 can be built");
+			//Thruster can be built
+			if(!h.checkPartBuilt(0)){ // first Thruster upgrade can be built
+				thrst1.setBackground(grn_btn);
+			} else if(!h.checkPartBuilt(1)){ // second Thruster upgrade can be built
+				thrst1.setBackground(blu_btn);
+				thrst2.setBackground(grn_btn);
+			} else if(!h.checkPartBuilt(2)){ // third Thruster upgrade can be built
+				thrst1.setBackground(blu_btn);
+				thrst2.setBackground(blu_btn);
+				thrst3.setBackground(grn_btn);
 			}
+		} else { // no Thruster upgrade can be built
+				thrst1.setBackground(red_btn);
+				thrst2.setBackground(red_btn);
+				thrst3.setBackground(red_btn);
 		} if(canBuildFuelReserves(h)){
 			//fuel reserve can be built (UI stuff)
 			if(!h.checkPartBuilt(3)){
-				System.out.println("Fuel Reserves Mk1 can be built");
+				fres1.setBackground(grn_btn);
 			} else if(!h.checkPartBuilt(4)){
-				System.out.println("Fuel Reserves Mk2 can be built");
+				fres1.setBackground(blu_btn);
+				fres2.setBackground(grn_btn);
 			} else if(!h.checkPartBuilt(5)){
-				System.out.println("Fuel Reserves Mk3 can be built");
+				fres1.setBackground(blu_btn);
+				fres2.setBackground(blu_btn);
+				fres3.setBackground(grn_btn);
 			}
+		} else {
+			fres1.setBackground(red_btn);
+			fres2.setBackground(red_btn);
+			fres3.setBackground(red_btn);
 		} if(canAddCrewMembers(h)){
 			//1 crew member can be added (UI stuff)
 			if(!h.checkCrewFull()){
@@ -97,6 +272,17 @@ public class BuildPhase implements JFrame{
 			int amountOfFuel = calcFuelQuantity(h);
 			//<amountOfFuel> units of fuel can be added (UI stuff)
 			System.out.println(amountOfFuel + " units of fuel can be added");
+		}
+	}
+	
+	private class ListenForButton implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == ckpt1) {
+				if(ckpt1.getBackground() == red_btn)
+					ckpt1.setBackground(grn_btn);
+				else
+					ckpt1.setBackground(red_btn);
+			}
 		}
 	}
 	
@@ -178,7 +364,7 @@ public class BuildPhase implements JFrame{
 	}
 	
 	private boolean canAddCrewMembers(Hand h){
-		return (h.numberOfCrewMembers >= 3);
+		return (h.numberOfCrewMembers() >= 3);
 	}
 	
 	private int calcFuelQuantity(Hand h){
@@ -190,6 +376,8 @@ public class BuildPhase implements JFrame{
 			return 2;
 		else if(h.numberOfFuel() == 5)
 			return 3;
+		else
+			return 0;
 	}
 	
 }
