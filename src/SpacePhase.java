cc14672497198cc24805be4d2b96c75d080ee0c8
andrewjyang <ayang2@zagmail.gonzaga.edu>
2018-05-02 00:05:51 -0700

    space phase

--- src/SpacePhase.java
+++ src/SpacePhase.java
@@ -1,68 +1,207 @@
 /**
  * class SpacePhase
+ *  cautious: distance * 1   fuel -2    accident 0%
+ *  normal:   distance * 1   fuel -1    accident 20% - crew members * 4
+ *  fast:     distance * 3   fuel -2    accident (20% - crew members * 4) * 2
  */
+import java.util.Scanner;
+
 public class SpacePhase {
     private int totalDistance;
     private int totalCrew;
+    private int totalFuel;
+    private int rocketTier;
+    private int[] tempScore;
 
-    private Hand[] players;
+    public enum SpeedTier {CAUTIOUS, NORMAL, FAST}
+
+    private double accidentChance;
+    private boolean accident;
+
+    private int speedOption;
 
     /**
      * SpacePhase constructor
      *
      * @param: players is the array of Hand objects with each object
-     *  corresponding to each player
+     * corresponding to each player
      * @return: n/a
      */
-    public SpacePhase(Hand[] players){
-        players[0].readScore();
-        System.out.println(players[0].readScore());
+    public SpacePhase(Hand[] players) {
+
+        for (int i = 0; i < players.length; i++) {
+
+            System.out.println("\nPLAYER " + (i+1) +"\n");
+            if(players[i].checkRocketBuilt()) {
+                tempScore = players[i].readScore();
+                totalCrew = tempScore[0];
+                totalFuel = tempScore[1];
+                rocketTier = tempScore[2];
+                totalDistance = 0;
+                speedOption = 0;
+
+                System.out.println("--- Player " + (i+1) + "'s  Rocket ---" );
+                System.out.println("total crew: " + totalCrew);
+                System.out.println("total fuel: " + totalFuel);
+                System.out.println("rocket tier: " + rocketTier);
+                System.out.println("total distance: " + totalDistance);
+
+                spaceTravel();
+            }
+            else {
+                System.out.println("ERROR: ROCKET NOT COMPLETE");
+            }
+        }
     }
 
-    // index of int array readScore()
-    // 0: crew members
-    // 1: fuel
-    // 2:
-    // constructor:
-    //  with objects Hand[] - array of hands(each player),
-    //      readScore()- returns array of integers representing ship parts
+    /**
+     * setPlayerSpeed() method
+     * sets the player speed based upon user input: CAUTIOUS, NORMAL, or FAST
+     * @param: n/a
+     * @return: n/a
+     */
+    private SpeedTier setPlayerSpeed(){
+        do{
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println(" --- Spaceship Speeds --- ");
+            int option = 1;
+            for (SpeedTier speed: SpeedTier.values()){
+                System.out.println(option + " ~ " + speed.name());
+                option++;
+            }
+            System.out.println("Please select a number corresponding to a speed setting: ");
+            try {
+                speedOption = keyboard.nextInt();
+                SpeedTier speedTier = SpeedTier.values()[speedOption - 1];
+                System.out.println("selected speed: " + speedTier);
+                return speedTier;
+            }
+            catch (Exception e) {
+                System.out.println("Please select a valid speed: ");
+                speedOption = keyboard.nextInt();
+                SpeedTier speedTier = SpeedTier.values()[speedOption - 1];
+                System.out.println("selected speed: " + speedTier);
+                return speedTier;
+            }
+        } while (speedOption == 0);
+    }
 
-    // tiers of shipparts 1-3
-    // increase fuel - static --> number turns
-    // increase crew members - static --> decreases chance of accident
-    // thrusters does affect distance --> base distance they move (Tier 1: move 1, Tier 2: move 2, Tier 3: move 3)
+    /**
+     * calculateFuel() method
+     * calculates the total amount of fuel remaining for each player
+     * @param: speed of type SpeedTier is the speed each player chooses to travel in space at
+     * @return: n/a
+     */
+    private int calculateFuel(SpeedTier speed){
+        if (speed == SpeedTier.CAUTIOUS){
+            totalFuel = totalFuel - 2;
+            System.out.println("remaining fuel: " + totalFuel);
+        }
+        else if (speed == SpeedTier.NORMAL){
+            totalFuel = totalFuel - 1;
+            System.out.println("remaining fuel: " + totalFuel);
+        }
+        else if (speed == SpeedTier.FAST){
+            totalFuel = totalFuel - 2;
+            System.out.println("remaining fuel: " + totalFuel);
+        }
+        else {
+            totalFuel = 0;
+        }
+        return totalFuel;
+    }
 
+    /**
+     * calculateAccident() method
+     * calculates the chance of an accident for each player
+     * @param: speed of type SpeedTier is the speed each player chooses to travel in space at
+     * @return: n/a
+     */
+    private boolean calculateAccident(SpeedTier speed) {
+        if (speed == SpeedTier.CAUTIOUS) {
+            accidentChance = 0.0;
+            System.out.println("accident chance: " + accidentChance + " %");
+            if(Math.random() * 100 < accidentChance){
+                System.out.println("GAME OVER: CRASH!");
+                accident = true;
+            }
+        }
+        else if (speed == SpeedTier.NORMAL) {
+            accidentChance = (20 - (totalCrew * 4));
+            System.out.println("accident chance: " + accidentChance + " %");
+            if(Math.random() * 100 < accidentChance){
+                System.out.println("GAME OVER: CRASH!");
+                accident = true;
+            }
+        }
+        else if (speed == SpeedTier.FAST){
+            accidentChance = ((20 - (totalCrew * 4)) * 2);
+            System.out.println("accident chance: " + accidentChance + " %");
+            if(Math.random() * 100 < accidentChance){
+                System.out.println("GAME OVER: CRASH!");
+                accident = true;
+            }
+        }
+        else { return false; }
+        return accident;
+    }
 
-    // *fuel = how many turns in space phase
-    // *crew = accident chance 1 crew = 20%, 2 crew = 15%
-    //  random 0-100, check 0-20, 0-15, etc..
+    /**
+     * calculateTotalDistance() method
+     * calculates the total distance each player travels
+     * @param: speed of type SpeedTier is the speed each player chooses to travel in space at
+     * @return: n/a
+     */
+    private int calculateTotalDistance(SpeedTier speed) {
+        if (speed == SpeedTier.CAUTIOUS){
+            totalDistance = totalDistance + rocketTier * 1;
+            System.out.println("total distance: " + totalDistance);
+        }
+        else if (speed == SpeedTier.NORMAL){
+            totalDistance = totalDistance + rocketTier * 1;
+            System.out.println("total distance: " + totalDistance);
+        }
+        else if (speed == SpeedTier.FAST){
+            totalDistance = totalDistance + rocketTier * 3;
+            System.out.println("total distance: " + totalDistance);
+        }
+        else {
+            totalDistance = 0;
+        }
+        return totalDistance;
+    }
 
-   // fuel = upper limit of for loop
-   // accident = pick a move
+    /**
+     * spaceTravel() method
+     * calls calculateFuel(), calculateAccident(), and calculateTotalDistance for each player
+     * spaceTravel() is the driver function for SpacePhase
+     * @param: n/a
+     * @return: n/a
+     */
+    private int spaceTravel(){
+        int finalDistance = 0;
+        int remainingFuel;
+        boolean crashAccident;
 
-   // fly space: tiers of thruster
-    // cautious: distance * 1   fuel -2    accident 0%
-    // normal:   distance * 1   fuel -1    accident 20% - crew members * 4
-    // fast:     distance * 3   fuel -2    accident (20% - crew members * 4) * 2
+        do {
+            SpeedTier speed = setPlayerSpeed();
+            if (speed == SpeedTier.CAUTIOUS) {
+                remainingFuel = calculateFuel(SpeedTier.CAUTIOUS);
+                crashAccident = calculateAccident(SpeedTier.CAUTIOUS);
+                finalDistance = calculateTotalDistance(SpeedTier.CAUTIOUS);
+            } else if (speed == SpeedTier.NORMAL) {
+                remainingFuel = calculateFuel(SpeedTier.NORMAL);
+                crashAccident = calculateAccident(SpeedTier.NORMAL);
+                finalDistance = calculateTotalDistance(SpeedTier.NORMAL);
+            } else if (speed == SpeedTier.FAST) {
+                remainingFuel = calculateFuel(SpeedTier.FAST);
+                crashAccident = calculateAccident(SpeedTier.FAST);
+                finalDistance = calculateTotalDistance(SpeedTier.FAST);
+            } else {
+                return 0;
+            }
+        } while (remainingFuel > 0 && !crashAccident);
 
-    // make accident chance integer
-
-
-    // flyspace() -- private
-    //  cycle through each players
-    //  if(players[0].checkRocketBuilt()) --> true, false: distance = 0
-    //  else --> you can't fly into space
-
-    //  .readScore() -> return [crewmembers, fuel, tier of thruster]
-
-    //   while fuel != 0
-    //   proceed cautious, normal, fast
-    //   subtract fuel... until empty
-    //   .recordfinaldistance()
-    //   nextplayer.
-    //
-
-    // calculateAccident()
-    // calculateDistance()
-
+        return finalDistance;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,64 @@
-import sun.jvm.hotspot.memory.Space;
-
+/**
+    SpacePhase testing
+ */
 public class Test {
     private Hand[] players;
 
     public static void main(String[] args) {
 
         Hand[] players = new Hand[2];
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < players.length; i++) {
             players[i] = new Hand();
         }
 
+        // PLAYER 1
+        // crew
+        players[0].addCommodity(0);
+        players[0].addCommodity(0);
+
+        // fuel
+        players[0].addCommodity(1);
+        players[0].addCommodity(1);
+        players[0].addCommodity(1);
+        players[0].addCommodity(1);
+
+        // rocket tier
         players[0].buildPart(0);
         players[0].buildPart(1);
-        players[0].addCommodity(0);
-        players[0].addCommodity(0);
-        players[0].addCommodity(1);
-        players[0].addCommodity(1);
-        players[0].addCommodity(1);
-        players[0].addCommodity(1);
-        int[] temp = players[0].readScore();
 
-        System.out.println(temp[2]);
-        System.out.println(players[0].getNumberFuel());
+        // complete rocket
+        players[0].buildPart(3);
+        players[0].buildPart(4);
+        players[0].buildPart(5);
+        players[0].buildPart(6);
+        players[0].buildPart(7);
+        players[0].buildPart(8);
+        players[0].buildPart(9);
 
-        SpacePhase space = new SpacePhase(players);
+        // PLAYER 2
+        // crew
+        players[1].addCommodity(0);
+        players[1].addCommodity(0);
 
+        // fuel
+        players[1].addCommodity(1);
+        players[1].addCommodity(1);
+        players[1].addCommodity(1);
+        players[1].addCommodity(1);
 
+        // rocket tier
+        players[1].buildPart(0);
+        players[1].buildPart(1);
+
+        // complete rocket
+        players[1].buildPart(3);
+        players[1].buildPart(4);
+        players[1].buildPart(5);
+        players[1].buildPart(6);
+        players[1].buildPart(7);
+        players[1].buildPart(8);
+        players[1].buildPart(9);
+
+        SpacePhase spaceship = new SpacePhase(players);
     }
 }
